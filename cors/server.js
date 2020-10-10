fc3c4394ba8bd15ecc08bc8b11c1a847dfee5c07
lzx <luanzhuxian@hotmail.com>
2020-10-10 21:55:04 +0800

    first commit

--- cors/server.js
+++ cors/server.js
@@ -1,10 +1,19 @@
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
 
 http.createServer(function (request, response) {
   console.log('request come', request.url)
 
-  const html = fs.readFileSync('test.html', 'utf8')
+  // 若不设置 urf8 编码，就会读取二进制格式，无法以字符串的形式返回
+  const html = fs.readFileSync(path.resolve(__dirname, 'test.html'), 'utf8')
+
+// console.log('path.resolve(__dirname, test.html)', path.resolve(__dirname, 'test.html'))
+// console.log('path.join(__dirname, test.html)', path.join(__dirname, 'test.html'))
+// console.log('path.resolve(__dirname, /cors/test.html)', path.resolve(__dirname, '/cors/test.html'))
+// console.log('path.join(__dirname, /cors/test.html)', path.join(__dirname, '/cors/test.html'))
+
+  // 若不设置 text/html，浏览器就会把返回当成纯文本
   response.writeHead(200, {
     'Content-Type': 'text/html'
   })
--- cors/server2.js
+++ cors/server2.js
@@ -1,15 +1,23 @@
 const http = require('http')
+const querystring = require('querystring')
 
 http.createServer(function (request, response) {
-  console.log('request come', request.url)
+    const [path, query] = request.url.split('?')
+    console.info('request come', request.url)
 
-  response.writeHead(200, {
-    'Access-Control-Allow-Origin': 'http://127.0.0.1:8888',
-    'Access-Control-Allow-Headers': 'X-Test-Cors',
-    'Access-Control-Allow-Methods': 'POST, PUT, DELETE',
-    'Access-Control-Max-Age': '1000'
-  })
-  response.end('123')
+    if (path === '/jsonp' && query) {
+        const { callback } = querystring.parse(query)
+        response.end(`${callback}(123)`)
+    } else {        
+        response.writeHead(200, {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'X-Test-Cors',
+            'Access-Control-Allow-Methods': 'POST, PUT, DELETE',
+            'Access-Control-Max-Age': '1000'
+        })
+        response.end('123')
+    }
+
 }).listen(8887)
 
 console.log('server listening on 8887')
